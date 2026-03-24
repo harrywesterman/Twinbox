b4c8d7bcbe3c669b7db6091c95ac5183935ba7a1
Harry Westerman <harrywesterman@gmail.com>
2026-03-24 12:18:52 +0100

    fix: correct path for ArgoCD root.yaml in installation script

--- gitops/install.sh
+++ gitops/install.sh
@@ -1,3 +1,3 @@
 kubectl create namespace argocd
 kubectl apply --server-side  -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v3.3.4/manifests/install.yaml
-kubectl apply -f gitops/argocd/root.yaml
+kubectl apply -f argocd/root.yaml
