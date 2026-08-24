a4923fffc4968e7ea587dd04f7238bc9edfc647f
twinbox-ci <twinbox@twinbox.local>
2026-08-24 12:27:31 +0000

    chore: bump image tags to sha-f950775 [skip ci]

--- wizard/setup-wizard.sh
+++ wizard/setup-wizard.sh
@@ -1011,7 +1011,7 @@ apply_educated_defaults() {
   PROXMOX_NODE="$(hostname)"
   PROXMOX_STORAGE_POOL="local-lvm"
   PROXMOX_FILE_DATASTORE="local"
-  TWINBOX_IMAGE_TAG="sha-e188cf9"
+  TWINBOX_IMAGE_TAG="sha-f950775"
 }
 
 run_apply_educated_defaults_with_gauge() {
@@ -1083,7 +1083,7 @@ EOF
   PROXMOX_NODE="$(hostname)"
   PROXMOX_STORAGE_POOL="local-lvm"
   PROXMOX_FILE_DATASTORE="local"
-  TWINBOX_IMAGE_TAG="sha-e188cf9"
+  TWINBOX_IMAGE_TAG="sha-f950775"
 }
 
 create_proxmox_api_user() {
